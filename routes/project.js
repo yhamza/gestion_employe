3e27a0a88dc61af2a1ca3f3f4e9caddaf761bd09
yassine hamza <114316966+yhamza@users.noreply.github.com>
2024-02-29 22:12:25 +0100

    Update project.js

--- routes/project.js
+++ routes/project.js
@@ -2,14 +2,14 @@ const router=require('express').Router()
 const project=require('../services/Project')
 const protect=require('../utils/protect')
 
-//create project mrgl
+//CREATE PROJECT
 router.post('/api/project/create',protect,project.create)
-//get all project mrgl
+//GET ALL PROJECT
 router.get('/api/project/findAll',protect,project.allProject)
-//delete project mrgl
+//DELETE PROJECT
 router.delete('/api/project/delete/:projectId',protect,project.delete)
 
-//  assign / unassign an employee to a project
+//  ASSIGN / UNASSIGN AN EMPLOYE TO A PROJECT
 router.post('/api/employe/update/assign',protect,project.assign)
 router.post('/api/employe/update/unassign',protect,project.unassign)
 
